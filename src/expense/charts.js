d15d8485317534d1edd59df052d0093f974ce05a
devakitella <devakitella@gmail.com>
2023-12-10 17:54:48 -0500

    Updated with Extra Chart

--- src/expense/charts.js
+++ src/expense/charts.js
@@ -3,6 +3,87 @@ import * as d3 from 'd3';
 
 const translate = (x, y) => `translate(${x},${y})`;
 
+const BubbleChart = ({ expenses, width, height, maxRadius }) => {
+  const svgRef = useRef();
+  useEffect(() => {
+    drawChart();
+  }, [expenses]);
+
+  function drawChart() {
+    // Remove the old svg
+    d3.select(svgRef.current)
+      .select('svg')
+      .remove();
+
+    // Create new svg
+    const svg = d3
+      .select(svgRef.current)
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height);
+
+    // Calculate the maximum amount for scaling the radius
+    const maxAmount = d3.max(expenses, d => d.amount);
+
+    // Scale for mapping the amount to radius
+    const radiusScale = d3
+      .scaleLinear()
+      .domain([0, maxAmount])
+      .range([0, maxRadius]);
+
+    // Initialize the simulation with the expenses array
+    const simulation = d3
+      .forceSimulation(expenses)
+      .force('charge', d3.forceManyBody().strength(5))
+      .force('x', d3.forceX().x(width / 2).strength(0.1))
+      .force('y', d3.forceY().y(height / 2).strength(0.1))
+      .force('collision', d3.forceCollide().radius(d => radiusScale(d.amount)))
+      .on('tick', ticked);
+
+      console.log(expenses)
+    function ticked() {
+      // Update the positions of the circles during each tick of the simulation
+      svg.selectAll('.bubble')
+        .attr('cx', d => d.x)
+        .attr('cy', d => d.y);
+      
+        svg.selectAll('.bubble-label')
+      .attr('x', (d) => d.x)
+      .attr('y', (d) => d.y)
+      .text((d) => d.categoryName);
+    }
+
+    const bubbles = svg
+  .selectAll('.bubble')
+  .data(expenses)
+  .enter()
+  .append('circle')
+  .attr('class', 'bubble')
+  .attr('r', d => radiusScale(d.amount))
+  .style('fill', 'blue'); // You can customize the fill color based on your data
+
+bubbles
+  .append('title')
+  .text(d => d.categoryName)
+  .attr('stroke', 'white');
+
+ // Add labels for the bubbles at the middle
+ svg.selectAll('.bubble-label')
+ .data(expenses)
+ .enter()
+ .append('text')
+ .attr('class', 'bubble-label')
+ .text((d) => d.categoryName)
+ .attr('text-anchor', 'middle')
+ .attr('alignment-baseline', 'middle')
+ .style('fill', 'black')
+ .style('font-size', '14px');
+}
+
+
+  return <svg ref={svgRef} />;
+};
+
 const Barchart = ({ expenses }) => {
   const ref = useRef();
 
@@ -166,11 +247,17 @@ const ChartContainer = ({ expenses }) => {
   return (
     <div>
       {hasExpenses && (
+        <>
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
         <Barchart expenses={expenses} />
         <LineChart expenses={expenses} />
-        <PieChart expenses={expenses} outerRadius={100} innerRadius={50} />
       </div>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <PieChart expenses={expenses} outerRadius={100} innerRadius={50} />
+      <BubbleChart expenses={expenses} width={400} height={400} maxRadius={50}/>
+
+    </div>
+        </>
       )}
     </div>
   );
